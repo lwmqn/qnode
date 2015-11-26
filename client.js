7efa068c9e26f938320a0512aa8ae940dd5d52de
Simen Li <simenkid@gmail.com>
2015-11-26 18:35:01 +0800

    testing

--- client.js
+++ client.js
@@ -4,7 +4,15 @@ var mqNode = new MqttNode('mnode_1', 1000, '1.1.1', '192.168.1.106', '00:0c:29:7
 var so = MqttNode.Generate('tempSensor');
 mqNode.hookSmartObject(so);
 //console.log(so);
-//console.log(mqNode.dumpObjectList());
+console.log(mqNode.dumpObjectList());
+
+mqNode.on('request', function (msg) {
+    console.log(msg);
+});
+
+mqNode.on('reg_rsp', function (msg) {
+    console.log(msg);
+});
 
 mqNode.on('connect', function () {
     console.log('connect');
@@ -23,8 +31,8 @@ mqNode.on('offline', function () {
 });
 
 mqNode.on('message', function (topic, message, packet) {
-    console.log(topic);
-    console.log(message);
+    //console.log(topic);
+    //console.log(message);
 });
 
 mqNode.on('error', function (err) {
