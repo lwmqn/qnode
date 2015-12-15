895f641a3c72f3a017f43475a8131c7231d9535b
Simen Li <simenkid@gmail.com>
2015-12-15 17:25:17 +0800

    notify and request are waiting for testing

--- client.js
+++ client.js
@@ -3,56 +3,33 @@ var devAttrs = {
     lifetime: 2000,
     version: '1.1.1',
     ip: '192.168.1.110',
-    mac: '00:0c:29:71:74:9f'
+    mac: '00:0c:29:71:74:ff'
 };
 
-var qnode = new MqttNode('mnode_1', devAttrs);
+var qnode = new MqttNode('mnode_1', devAttrs, 'tempSensor');
+qnode.writeResrc('tempSensor', 0, 'sensorValue', 300);
 
-// what client wants
-//      qnode = new MqttNode('mnode_1', devAttrs, 'templateName');  // so is ready
-//      qnode.addIObject(oid, [iid,] ridSet);
-
-// qnode = new MqttNode('mnode_1', devAttrs);
-// qnode.bindSmartObject(name, iObjects);
-
-// qnode = new MqttNode('mnode_1', devAttrs);
-// qnode.bindSmartObject(name);
-// qnode.addIObject(oid, [iid,] ridSet);
-
-// so = new MqttNode.SmartObject(name, iObjects);
-// qnode.bind(so);
-
-var so = MqttNode.SmartObject.getTemplate('tempSensor');
-var newObject = new MqttNode.SmartObject.IObject('device', { manuf: 'sivann2' });
-
-qnode.bindSo(so);
-so.tempSensor[0].sensorValue = 300;
-//so.device[0].manuf = 'sivannYYYYYY';
 //so.addIObjects(newObject);
-//console.log(so);
+
 console.log(qnode.objectList());
 
 qnode.on('connect', function () {
     console.log('connect');
-    //console.log(so);
-    // qnode.pubRegister().then(function (res) {
-    //     console.log(res);
-    // });
 
-    setTimeout(function() {
-        setInterval(function () {
-            so.tempSensor[0].sensorValue = 67 + Math.floor(Math.random() * 100 + 1);
-        }, 6000);
-    }, 10000);
+    // setTimeout(function() {
+    //     setInterval(function () {
+    //         var val = 67 + Math.floor(Math.random() * 100 + 1);
+    //         qnode.writeResrc('tempSensor', 0, 'sensorValue', val);
+    //     }, 6000);
+    // }, 10000);
 
-
-
-    setTimeout(function () {
-        setInterval(function () {
-            console.log('xxxxx');
-            console.log(qnode.getIObject('device', 0).getResrcAttrs(0));
-        }, 6000);
-    }, 10000);
+    // setTimeout(function () {
+    //     setInterval(function () {
+    //         console.log('xxxxx');
+    //         //console.log(qnode.getIObject('device', 0).getResrcAttrs(0));
+    //         console.log(qnode.getAttrs('device', 0, 0));
+    //     }, 6000);
+    // }, 10000);
 });
 
 qnode.on('request', function (msg) {
@@ -60,11 +37,53 @@ qnode.on('request', function (msg) {
 });
 
 qnode.on('reg_rsp', function (msg) {
+    console.log('reg_rsp');
+    console.log(msg);
+    setTimeout(function () {
+        // qnode.addIObject('testoid', {
+        //     tr1: 1,
+        //     tr2: 'hello'
+        // });
+
+        // qnode.pubUpdate({
+        //     lifetime: 999,
+        //     ip: '661.222.333.444',
+        //     //mac: 'xxx'
+        //     //objList: qnode.objectList()
+        // }).fail(function(err) {
+        //     console.log(err);
+        // });
+
+        qnode.pingServer();
+        // qnode.pubDeregister(;)
+    }, 5000);
+
+});
+
+
+qnode.on('dereg_rsp', function (msg) {
+    console.log('dereg_rsp');
+    console.log(msg);
+});
+
+
+qnode.on('ping_rsp', function (msg) {
+    console.log('ping_rsp');
+    console.log(msg);
+});
+
+qnode.on('announce', function (msg) {
+    console.log('announce');
     console.log(msg);
 });
 
 qnode.on('reconnect', function () {
     console.log('reconnect');
+});
+
+qnode.on('update_rsp', function (rsp) {
+    console.log('update_rsp *****');
+    console.log(rsp);
 });
 
 qnode.on('close', function () {
