73c3708d64fa071de67ede7b2699f46deaced9ba
Simen Li <simenkid@gmail.com>
2015-12-16 18:46:52 +0800

    last 2 APIs to test: writeReq and observeReq

--- client.js
+++ client.js
@@ -1,4 +1,7 @@
-var MqttNode = require('./index');
+var util = require('util'),
+    mutils = require('./lib/utils/mutils.js'),
+    MqttNode = require('./index');
+
 var devAttrs = {
     lifetime: 2000,
     version: '1.1.1',
@@ -9,19 +12,54 @@ var devAttrs = {
 var qnode = new MqttNode('mnode_1', devAttrs, 'tempSensor');
 qnode.writeResrc('tempSensor', 0, 'sensorValue', 300);
 
+qnode.addIObject('tempSensor', 1, {
+    sensorValue: 1200,
+    units: 'mCel',
+    minMeaValue: 10,
+    maxMeaValue: 2000,
+    minRangeValue: 0,
+    maxRangeValue: 4000,
+    resetMinMaxMeaValues: { exec: function (p1, p2, p3, callback) {
+        console.log(arguments);
+
+        var x = p2 + p3,
+            rsp = {
+                status: 204,
+                data: x
+            };
+
+        if (typeof p2 !== 'number') {
+            rsp.status = 400;
+            rsp.data = null;
+        }
+
+        callback(false, rsp);
+    } }
+});
+
 //so.addIObjects(newObject);
 
+// console.log(qnode.so);
 console.log(qnode.objectList());
+
+// qnode.dump(function (err, r) {
+//     // console.log(r);
+//     r.so = mutils.soWithStringKeys(r.so);
+//     console.log(util.inspect(r, { showHidden: true, depth: null }));
+// });
 
 qnode.on('connect', function () {
     console.log('connect');
 
-    // setTimeout(function() {
-    //     setInterval(function () {
-    //         var val = 67 + Math.floor(Math.random() * 100 + 1);
-    //         qnode.writeResrc('tempSensor', 0, 'sensorValue', val);
-    //     }, 6000);
-    // }, 10000);
+    setTimeout(function() {
+        setInterval(function () {
+            var val = 67 + Math.floor(Math.random() * 100 + 1);
+            qnode.writeResrc('tempSensor', 0, 'sensorValue', val).done(function (val) {
+                console.log('WRITE SELF');
+                console.log(val);
+            });
+        }, 6000);
+    }, 10000);
 
     // setTimeout(function () {
     //     setInterval(function () {
@@ -72,9 +110,18 @@ qnode.on('ping_rsp', function (msg) {
     console.log(msg);
 });
 
+qnode.on('notify_rsp', function (msg) {
+    console.log(' [[[[[[[[[ notify_rsp ]]]]]]]]]]]');
+    console.log(msg);
+});
+
 qnode.on('announce', function (msg) {
     console.log('announce');
     console.log(msg);
+
+    // qnode.so.notify('device', 0, 'manuf', 'Fly_Bird');
+    // qnode.so.notify('device', 0, 'manufx', 'Fly_Bird');
+    // qnode.so.notify('tempSensor', 1, { resetMinMaxMeaValues: 'abcdde', sensorValue: 888 } );
 });
 
 qnode.on('reconnect', function () {
