08f20e22410b2d8fa116147ed602097f44eda80c
Simen Li <simenkid@gmail.com>
2016-01-18 18:26:21 +0800

    refactoring, reduce code size

---
+++ APIs.js
@@ -0,0 +1,48 @@
+// new(n) v
+// encrypt(msg) v
+// decrypt(msg) v
+// changeIp(ip)
+// initResrc(...) v
+// getAttrs(...) v
+// setAttrs(...)
+// readResrc(oid, iid, rid, callback)
+// writeResrc(oid, iid, rid, value, callback)
+// execResrc(oid, iid, rid, argus, callback)
+// connect(url, opts)
+// close(callback)
+// pubRegister(callback)
+// pubDeregister(callback)
+// pingServer(callback)
+// pubUpdate(devAttrs, callback)
+
+// pubResponse(rsp, callback)
+// publish(topic, message, options, callback)
+// subscribe(topic, qos, callback)
+// _pubReq(intf, data, callback)
+// _target(oid, iid, rid)
+// _id(intf) v
+// _dumpObj(...)
+// _rd(chk, oid, iid, rid, callback)
+
+// _rawHdlr(conn, topic, message)
+// _reqHdlr(msg)
+// _lfUp(enable)
+// _chkResrc(oid, iid, rid, currVal)
+// enableReport(oid, iid, rid, attrs)
+// disableReport(oid, iid, rid)
+// _tmout(key, delay)
+// _path(path)
+// local function rm(tbl, pred)
+// local function chk()
+// tm.start()
+// tm.stop()
+// tm.set(tid)
+// tm.setTimeout(fn, delay, ...)
+// tm.setInterval(fn, delay, ...)
+// tm.clear(tobj)
+// _evtb(ev)
+// emit(ev, ...)
+// once(ev, listener)
+// removeAll(ev)
+// remove(ev, listener)
+
