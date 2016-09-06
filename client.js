d8eb90c386de7f1cc9821f44341c226eafb78b90
Simen Li <simenkid@gmail.com>
2016-09-06 18:40:02 +0800

    fix DEBUG print bug when descryption fails

--- client.js
+++ client.js
@@ -67,33 +67,33 @@ var qnode = new MqttNode('test_node_01', so, devAttrs);
 /*********************************************/
 /*** The custom encrption/decryption       ***/
 /*********************************************/
-// qnode.encrypt = function (msg, callback) {
-//     var msgBuf = new Buffer(msg),
-//         cipher = crypto.createCipher('aes128', 'mypassword'),
-//         encrypted = cipher.update(msgBuf, 'binary', 'base64');
-//     try {
-//         encrypted += cipher.final('base64');
-//         callback(null, encrypted);
-//     } catch (e) {
-//         callback(e);
-//     }
+qnode.encrypt = function (msg, callback) {
+    var msgBuf = new Buffer(msg),
+        cipher = crypto.createCipher('aes128', 'mypassword'),
+        encrypted = cipher.update(msgBuf, 'binary', 'base64');
+    try {
+        encrypted += cipher.final('base64');
+        callback(null, encrypted);
+    } catch (e) {
+        callback(e);
+    }
 
-// };
+};
 
-// qnode.decrypt = function (msg, callback) {
-//     msg = msg.toString();
-//     var decipher = crypto.createDecipher('aes128', 'mypassword'),
-//         decrypted = decipher.update(msg, 'base64', 'utf8');
+qnode.decrypt = function (msg, callback) {
+    msg = msg.toString();
+    var decipher = crypto.createDecipher('aes128', 'mypassword'),
+        decrypted = decipher.update(msg, 'base64', 'utf8');
 
-//     try {
-//         decrypted += decipher.final('utf8');
-//         callback(null, decrypted);
-//     } catch (e) {
-//         // log 'decrytion fails'
-//         console.log('decrytion fails.');
-//         callback(e);
-//     }
-// };
+    try {
+        decrypted += decipher.final('utf8');
+        callback(null, decrypted);
+    } catch (e) {
+        // log 'decrytion fails'
+        console.log('decrytion fails.');
+        callback(e);
+    }
+};
 
 
 /*************************************************************************************************/
